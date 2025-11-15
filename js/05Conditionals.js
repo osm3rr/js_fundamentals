e0e1f4cf52d7a8aecbdab082ea33c7e51a9a4daa
osm3rr <osm3rr@adakdemy.com>
2025-11-15 18:04:01 -0400

    conditionals

---
+++ js/05Conditionals.js
@@ -0,0 +1,12 @@
+// condiotionals in js
+const age = 20;
+
+// if-else statement
+if (age < 18) {
+    alert("Minor");
+} else if (age >= 18 && age < 65) {
+    alert("Adult");
+} else {
+    alert("Senior");
+}
+
