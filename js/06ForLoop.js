e58e62dfa3baea928786b7321861a36719d7d082
osm3rr <osm3rr@adakdemy.com>
2025-11-16 09:34:19 -0400

    for loop

---
+++ js/06ForLoop.js
@@ -0,0 +1,36 @@
+// for loops in JavaScript
+
+// Basic for loop
+for (let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}
+
+// Looping through an array
+// const fruits = ["apple", "banana", "cherry"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log("Fruit: " + fruits[i]);
+// }
+
+// // Nested for loop
+// for (let i = 1; i <= 3; i++) {
+//     for (let j = 1; j <= 2; j++) {
+//         console.log("i: " + i + ", j: " + j);
+//     }
+// }
+
+// // Using for loop with break statement
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         console.log("Breaking the loop at i = " + i);
+//         break;
+//     }
+//     console.log("Current value of i: " + i);
+// }
+
+// // Using for loop with continue statement
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) {
+//         continue; // Skip even numbers
+//     }
+//     console.log("Odd number: " + i);
+// }
